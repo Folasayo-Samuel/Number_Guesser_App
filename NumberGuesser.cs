ada11adf85aa1ed1eec236919dba436096ec27d1
Folasayo Samuel <folasayosamuel@gmail.com>
2022-07-21 15:00:53 +0100

    update

---
+++ NumberGuesser.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace NumberGuesser
+{
+	class Program
+	{
+		static void Main(string[]args)
+		
+		{
+			// Using a placeholder in C#
+			string name = "Folasayo Samuel";
+			int age = 24;
+			bool isMale = true;
+			
+			Console.WriteLine("{0} is {1}\nis Samuel a male? {2}", name,age,isMale);
+		}
+	}
+}
